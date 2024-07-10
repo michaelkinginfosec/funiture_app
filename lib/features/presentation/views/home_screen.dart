304b1de971f3f7aa32342b91ea1f17108c56e330
michaelkinginfosec <osundemichael7@gmail.com>
2024-07-10 14:25:38 +0100

    update

--- lib/features/presentation/views/home_screen.dart
+++ lib/features/presentation/views/home_screen.dart
@@ -6,19 +6,61 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.black,
       body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
           child: Column(
-        children: [
-          Text(
-            'Furniture \n in your style',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.normal,
-            ),
-            textAlign: TextAlign.left,
+            children: [
+              const Text(
+                'Furniture \n in your style',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.normal,
+                ),
+                textAlign: TextAlign.left,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: 150,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  gradient: const LinearGradient(
+                    colors: [
+                      Color.fromARGB(255, 29, 133, 83),
+                      Color.fromARGB(255, 74, 187, 132),
+                    ],
+                  ),
+                ),
+                child: Positioned(
+                  top: 50,
+                  child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            'Elegant\n white chir',
+                          ),
+                          SizedBox(
+                            height: 12,
+                          ),
+                          Text(
+                            '\$200',
+                          ),
+                        ],
+                      ),
+                      Image.asset('asset/images/')
+                    ],
+                  ),
+                ),
+              )
+            ],
           ),
-        ],
-      )),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:funiture_app/features/presentation/views/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
