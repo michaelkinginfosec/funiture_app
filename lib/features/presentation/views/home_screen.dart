13043026222de9df8a0f996f7c43d88556db50a0
michaelkinginfosec <osundemichael7@gmail.com>
2024-07-10 13:56:03 +0100

    update

--- lib/features/presentation/views/home_screen.dart
+++ lib/features/presentation/views/home_screen.dart
@@ -5,6 +5,20 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Text(
+            'Furniture \n in your style',
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.normal,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ],
+      )),
+    );
   }
 }
